cd6e96ae7b148473164936693e2caacbccbf28f4
Ilya Rodionov <ris58h@gmail.com>
2022-12-12 19:22:28 +0300

    Remove unused methods

--- app/src/main/java/ris58h/galaxyfinder/widget/FinderWidget.kt
+++ app/src/main/java/ris58h/galaxyfinder/widget/FinderWidget.kt
@@ -6,34 +6,20 @@ import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.widget.RemoteViews
 
-/**
- * Implementation of App Widget functionality.
- */
 class FinderWidget : AppWidgetProvider() {
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
-        // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId)
         }
     }
-
-    override fun onEnabled(context: Context) {
-        // Enter relevant functionality for when the first widget is created
-    }
-
-    override fun onDisabled(context: Context) {
-        // Enter relevant functionality for when the last widget is disabled
-    }
 }
 
 internal fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
-    // Construct the RemoteViews object
     val views = RemoteViews(context.packageName, R.layout.finder_widget)
 
     val launchIntent = context.packageManager.getLaunchIntentForPackage("com.samsung.android.app.galaxyfinder")
     val pendingIntent = PendingIntent.getActivity(context, 0, launchIntent, PendingIntent.FLAG_IMMUTABLE)
     views.setOnClickPendingIntent(R.id.appwidget, pendingIntent)
 
-    // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
 }
